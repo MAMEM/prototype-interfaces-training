55ef638c00bd2405e7c91d6f1aa9ff17cd99d2a2
Tasos Papazoglou - Chalikias <tpapazoglou@iti.gr>
2017-01-10 11:53:01 +0200

    Initial commit

--- user.js
+++ user.js
@@ -1,26 +1,11 @@
 var user = [];
 
 // Mandatory fields. Please input your details
-user.firstName = "Anastasios";
-user.lastName = "Papazoglou - Chalikias";
-user.email = "sushi.pervert@gmail.com";
-user.password = "1234mamem1234";
 
-/*user.firstName = "Tester";
-user.lastName = "Von Test";
-user.email = "tpapazoglou@iti.gr";
-user.password = "1234mamem1234";*/
-
-/*user.firstName = "Doggie";
-user.lastName = "Mc Dogface";
-user.email = "tasos@tpapazoglou.com";
-user.password = "1234mamem1234";*/
-
-
-/*user.firstName = "TestUser";
+user.firstName = "TestUser";
  user.lastName = "Delete me";
  user.email = "g@g.gr";
- user.password = "1234mamem1234";*/
+ user.password = "1234mamem1234";
 
 // Don't change that.
 user.name = user.firstName + " " + user.lastName;
