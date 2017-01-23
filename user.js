e6507b9090e87334decb9056593c590c53bb1150
Tasos Papazoglou - Chalikias <tpapazoglou@iti.gr>
2017-01-23 15:56:10 +0200

    Added Gender, Age and Tech Exp fields for a user.

--- user.js
+++ user.js
@@ -1,10 +1,15 @@
 var user = [];
 
 // Mandatory fields. Please input your details
+
 user.firstName = "TestUser";
 user.lastName = "Delete me";
 user.email = "g@g.gr";
 user.password = "1234mamem1234";
+
+user.gender = "m";  // Gender: m: male, f: female
+user.age = 31;      // Age
+user.exp = "";      // The technical experience of the user.
 
 // Don't change that.
 user.name = user.firstName + " " + user.lastName;
